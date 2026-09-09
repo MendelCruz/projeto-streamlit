a66df53399fdc3f7c83b0dfb80040f083657dcfe
MendelCruz <109176092+MendelCruz@users.noreply.github.com>
2026-09-09 01:02:22 -0300

    Arquivos enviados

---
+++ Cadastro.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+from datetime import date
+
+def gravar_dados(nome, data_nasc, tipo ):
+    if nome and data_nasc <= date.today() and tipo:
+        with open ("clientes.csv", "a", encoding="utf-8") as file:
+            file.write(f"{nome},{data_nasc}, {tipo}\n")
+        st.session_state["sucesso"] = True
+    else:
+        st.session_state["sucesso"] = False
+
+
+st.set_page_config(
+    page_title="Cadastro de Clientes",
+    page_icon="📔"
+)
+
+st.title("Cadastro de Clientes")
+st.divider()
+
+nome = st.text_input("Digite o nome do Cliente", 
+                     key="nome_cliente")
+
+dt_nascimento = st.date_input("Digite a data de Nascimento",
+                                format="DD/MM/YYYY",key="dt_nascimento")
+
+tipo = st.selectbox("Tipo do Cliente",
+                    ["PJ","PF"],
+                    index=None)
+
+btn_cadastrar = st.button("Cadastrar",
+                          on_click=gravar_dados,
+                          args=[nome, dt_nascimento, tipo])
+
+if btn_cadastrar:
+    if st.session_state["sucesso"]:
+        st.success("Cliente cadastrado com sucesso",
+                   icon="👌")
+    else:
+        st.error("Problema no cadastro",
+                 icon="❌")
